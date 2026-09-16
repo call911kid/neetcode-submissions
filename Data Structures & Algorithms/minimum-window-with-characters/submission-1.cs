095bbce330536ae507ac54149f167806d1a2e232
Mustafa Hassan <91998433+call911kid@users.noreply.github.com>
2026-09-17 02:48:49 +0300

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.cs
@@ -0,0 +1,51 @@
+public class Solution
+{
+    public string MinWindow(string s, string t)
+    {
+        int[] freq2 = new int[125];
+        foreach(char c in t) freq2[c]++;
+
+        int L = 1, R = s.Length;
+        string ans = "";
+        while (L <= R)
+        {
+            int mid = L + (R - L) / 2;
+            string check = CheckInclusion(s, mid, freq2);
+            if (check == null) L = mid + 1;
+            else
+            {
+                ans = check;
+                R = mid - 1;
+            }
+            //Console.WriteLine($"{mid} = {check}");
+        }
+
+        return ans;
+    }
+    public string CheckInclusion(string s1, int len, int[] freq2)
+    {
+        int[] freq1 = new int[125];
+
+        for (int i = 0; i < s1.Length; i++)
+        {
+            if (i < len - 1)
+            {
+                freq1[s1[i]]++;
+
+                continue;
+            }
+            freq1[s1[i]]++;
+            bool ok = true;
+            for (int j = 65; j <= 122; j++)
+            {
+                if (freq1[j] < freq2[j]) ok = false;
+            }
+
+            if (ok) return s1.Substring(i - len + 1, len);
+            freq1[s1[i - len + 1]]--;
+
+        }
+
+        return null;
+    }
+}
