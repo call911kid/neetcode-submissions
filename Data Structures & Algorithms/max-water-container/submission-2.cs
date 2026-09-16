2e333f331947b201069debe5692b7d77feaf45e5
Mustafa Hassan <91998433+call911kid@users.noreply.github.com>
2026-09-16 16:30:04 +0300

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.cs
@@ -0,0 +1,51 @@
+public class Solution
+{
+    public int MaxArea(int[] heights)
+    {
+        int[] L = new int[heights.Length + 1];
+        int[] R = new int[heights.Length + 1];
+        L[0] = heights[0];
+        R[0] = heights[heights.Length - 1];
+        for (int i = 1; i < heights.Length; i++) L[i] = Math.Max(L[i - 1], heights[i]);
+        for (int i = 1; i < heights.Length; i++) R[i] = Math.Max(R[i - 1], heights[heights.Length - i - 1]);
+       
+        int max = 0;
+        for (int i = 0; i < heights.Length; i++)
+        {
+            int left = UpperBound(L, 0, i, heights[i]);
+            int right = UpperBound(R, 0, heights.Length - i - 1, heights[i]);
+
+            int l = Math.Min(heights[i], heights[left]);
+            int r = Math.Min(heights[i], heights[heights.Length - i - 1]);
+
+            max = Math.Max(max, Math.Max((i - left) * l, (heights.Length - right - i - 1) * r));
+        }
+
+        return max;
+
+
+    }
+    private int UpperBound(int[] arr, int L, int R, int target)
+    {
+        int ans = -1;
+        while (L <= R)
+        {
+            int mid = L + (R - L) / 2;
+
+            if (arr[mid] < target)
+            {
+
+                L = mid + 1;
+            }
+            else
+            {
+                ans = mid;
+                R = mid - 1;
+            }
+
+        }
+        Console.WriteLine(ans);
+
+        return ans;
+    }
+}
