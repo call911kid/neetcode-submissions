551fe3d027715e807418cd986130e77432e6bc80
Mustafa Hassan <91998433+call911kid@users.noreply.github.com>
2026-09-17 21:07:10 +0300

    Add: number-of-one-bits - submission-1

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-1.cs
@@ -0,0 +1,12 @@
+public class Solution
+{
+    public int HammingWeight(uint n)
+    {
+        int count = 0;
+
+        for (; n > 0; n &= n - 1, count++) ;
+        
+        return count;
+        
+    }
+}
